dd92963e64d8c0d874aed945c43224511bafad6d
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-19 13:17:02 +0000

    implemented tcp server

--- src/tcp/Server.java
+++ src/tcp/Server.java
@@ -1,12 +1,13 @@
 package tcp;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     private static final int PORT = 8080;
 
-    private static void handleClient(Socket client) {
+    private static void handleClient(Socket client) throws IOException {
         try {
             byte[] request = new byte[1024];
 
@@ -17,8 +18,33 @@ public class Server {
             System.out.println("processing complete....");
 
             String response = "Hello World!";
-        } catch (IOException e) {
+            client.getOutputStream().write(response.getBytes());
+        } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+            client.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+
+        try {
+            Socket client = serverSocket.accept();
+            System.out.println("new client connected!");
+            Thread clientThread = new Thread(() -> {
+                try {
+                    handleClient(client);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            clientThread.start();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            serverSocket.close();
         }
     }
 }
