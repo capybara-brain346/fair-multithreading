242b98e990e22ee6e8182f3b548c586bf5200baa
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-19 13:11:44 +0000

    added handle client

--- src/tcp/Server.java
+++ src/tcp/Server.java
@@ -1,4 +1,24 @@
 package tcp;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Server {
+    private static final int PORT = 8080;
+
+    private static void handleClient(Socket client) {
+        try {
+            byte[] request = new byte[1024];
+
+            client.getInputStream().read(request);
+            System.out.println("Processign request");
+            Thread.sleep(3000);
+
+            System.out.println("processing complete....");
+
+            String response = "Hello World!";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
