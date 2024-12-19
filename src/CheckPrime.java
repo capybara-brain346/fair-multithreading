bd16f4441c0ea5e8dc930add88d76d6b480ff4b5
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-19 10:14:22 +0000

    complete check prime seq

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -1,21 +1,36 @@
 
 public class CheckPrime {
 
+    public int maxInt = Integer.MAX_VALUE;
+    public int totalPrimeNumbers = 0;
+
     private boolean checkPrime(int x) {
-        if ((x & 1) == 0)  {
+        if ((x & 1) == 0) {
             return false;
         }
 
-        for (int i = 3; i * i <= x; i += 2) {
+        for (int i = 3; i <= (int) Math.pow(x, 0.5); i += 2) {
             if (x % i == 0) {
                 return false;
             }
         }
+        totalPrimeNumbers++;
+        return true;
     }
 
     public static void main(String[] args) {
-        int maxInt = Integer.MAX_VALUE;
-        int totalPrimeNumbers = 0;
+
+        CheckPrime checkPrime = new CheckPrime();
+
+        final long startTime = System.currentTimeMillis();
+
+        for (int i = 3; i <= checkPrime.maxInt; i++) {
+            checkPrime.checkPrime(i);
+        }
+
+        final long elapsedTimeMillis = System.currentTimeMillis() - startTime;
+
+        System.out.println("Total prime numbers: " + checkPrime.totalPrimeNumbers + 1 + " in " + elapsedTimeMillis + "ms");
 
     }
 }
